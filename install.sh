3ae6f072786f996537d68f1a40c7f407ac6672e3
Daniel Randall <dgrandall@me.com>
2017-03-20 19:17:48 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="zshrc oh-my-zsh config xinitrc Xdefaults Xauthority"    # list of files/folders to symlink in homedir
+olddir=~/old_dotfiles             # old dotfiles backup directory
+files="zshrc config/bspwm config/sxhkd xinitrc Xdefaults"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -24,8 +24,8 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    echo "Moving any existing dotfiles from ~.$file to $olddir"
+    mv ~/.$file ~/old_dotfiles/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
