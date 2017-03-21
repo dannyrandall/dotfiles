700f5c48511fd152d8591659021eea1ea955b8d8
Daniel Randall <dgrandall@me.com>
2017-03-21 15:09:29 -0600

    added comments

--- backgrounds.sh
+++ backgrounds.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+###############################
+# Unsplash Collection Downloader
+##### from Daniel Randall
+###############################
+#
+# This script will download the full versions of images from a collection on unsplash.com
+# Change the url's collection ID (defaults to mine, 620389) to change the collection that is downloaded.
+# You can also change where they are downloaded to by changing $folder and $old_folder.
+# Images will be saved with .jpg extension, in the form of bg<image number>.jpg
+#
+
+## variables
 collectionURL="https://unsplash.com/collections/620389/"  # url to download backgrounds from
 folder="$HOME/pictures/backgrounds" #folder to save to
 oldFolder="$HOME/pictures/old_backgrounds" # folder to move whatever was in $folder before to
+##
 
 echo "Moving old backgrounds to $oldFolder"
 mkdir -p $oldFolder; cd $oldFolder; rm -r $oldFolder/* ; mv $folder/* $oldFolder # clear $oldFolder, move stuff in $folder to $oldFolder
