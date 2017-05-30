5c81fb387c78a76b704e736feb4f54ae3db46982
Daniel Randall <dgrandall@me.com>
2017-05-30 17:02:09 -0600

    delete the old branches

--- byuoitavUpdate.sh
+++ byuoitavUpdate.sh
@@ -13,5 +13,5 @@ parentFolder="$GOPATH/src/github.com/byuoitav"
 
 for folder in $parentFolder/*; do 
 	printf "updating $(basename $folder)\n\t"	
-	cd $folder && git pull	
+	cd $folder && git pull -p	
 done
